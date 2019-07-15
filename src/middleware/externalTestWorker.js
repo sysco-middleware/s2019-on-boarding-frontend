c4d4637b4d8017e390e68298e892c1e708535776
Ellina Ivleva <ivlellina@gmail.com>
2019-07-15 08:20:51 +0200

    Forms styling with material ui

--- src/middleware/externalTestWorker.js
+++ src/middleware/externalTestWorker.js
@@ -1,5 +1,5 @@
 const { Client, logger } = require("camunda-external-task-client-js");
-const nodemailer = require("nodemailer");
+//const nodemailer = require("nodemailer");
 const fetch = require("node-fetch");
 
 //import { CALL_API, Schemas } from '../middleware/api';
@@ -48,8 +48,8 @@ client.subscribe("EmailReminder", async function({ task, taskService }) {
     
         let mailOptions = {
             from: '"Camunda Web" <admin@sysco.no>',
-            to: 'magnus.ihle@gmail.com',
-            subject: 'Welcome Email',
+            to: 'ivlellina@gmail.com',
+            subject: 'Camunda Reminder',
             html: '<h1>You have not finished your assigned task in Camunda.</h1><br><a href="http://localhost:3000/tasklist/">Please click this link to finish your assigned task.</a>'
         };
     
@@ -62,7 +62,63 @@ client.subscribe("EmailReminder", async function({ task, taskService }) {
     });
 }
 
-  
-
   await taskService.complete(task);
+  
 });
+
+client.subscribe("EmailreminderAdFin", async function({ task, taskService }) {
+  
+    //  Nodemailer
+    console.log("Email reminder AdFin");
+    
+    var processId = task.processInstanceId;
+    var secondLink;
+  
+  
+    fetch('http://camunda-mysql:8080/engine-rest/task?processInstanceId=' + processId)
+    .then(response => response.json())
+    .then(data => {
+        var taskId = data[0].id;
+        var processDefinitionId = data[0].processDefinitionId;
+  
+        var link = processDefinitionId + '/' + taskId;
+        secondLink = link;
+        logLink();
+    })
+    .catch(error => {
+        console.log(error);
+    })
+  
+    function logLink() {
+      console.log("Link: " + secondLink);
+  
+      nodemailer.createTestAccount((err, account) => {
+          let transporter = nodemailer.createTransport({
+              host: 'smtp.googlemail.com', 
+              port: 465, 
+              secure: true,
+              auth: {
+                  user: 'sysco.sommer@gmail.com', 
+                  pass: 'Sommer2019' 
+              }
+          });
+      
+          let mailOptions = {
+              from: '"Camunda Web" <admin@sysco.no>',
+              to: 'ivlellina@gmail.com',
+              subject: 'Camunda Reminder',
+              html: '<h1>You have not finished your assigned task in Camunda.</h1><br><a href="http://localhost:3000/tasklist/">Please click this link to finish your assigned task.</a>'
+          };
+      
+          transporter.sendMail(mailOptions, (error, info) => {
+              if (error) {
+                  return console.log(error);
+              }
+              console.log('Epost sendt: %s', info.messageId);
+          }); 
+      });
+  }
+  
+    await taskService.complete(task);
+    
+  });
