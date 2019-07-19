31c503fda711faef1883336482f763ebbc039d66
magnusihle <s315276@stud.hioa.no>
2019-07-19 12:36:16 +0200

    merge

--- src/components/forms/onboard/index.js
+++ src/components/forms/onboard/index.js
@@ -2,5 +2,5 @@ export { default as startOnboard } from './startOnboard'
 export { default as sdOnboard } from './sdOnboard'
 export {default as hrOnboard} from './hrOnboard'
 export {default as ssdOnboard} from './ssdOnboard'
-export { default as adFinOnboard } from './adFinOnboard'
+export { default as adFinOnboard } from './AdFinOnboard'
 
