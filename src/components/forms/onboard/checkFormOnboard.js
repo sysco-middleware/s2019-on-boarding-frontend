72ff0e503fb6ada3ecb258365da77cd0e7cb4626
Ellina Ivleva <ivlellina@gmail.com>
2019-07-19 16:21:54 +0200

    Generating PDF

--- src/components/forms/onboard/checkFormOnboard.js
+++ src/components/forms/onboard/checkFormOnboard.js
@@ -1,7 +1,6 @@
 import React from "react";
-import { Field, reduxForm , } from "redux-form";
 import { connect } from "react-redux";
-
+import { Field, reduxForm, formValueSelector } from "redux-form";
 import {
   Page,
   Text,
@@ -18,148 +17,152 @@ import {
   CheckboxField
 } from "react-semantic-redux-form";
 import "react-datepicker/dist/react-datepicker.css";
-//import console = require("console");
-
+import PDF from "../../../middleware/PDF";
 
 const styles = StyleSheet.create({
   page: { backgroundColor: "" },
   section: { color: "black", textAlign: "left", margin: 30 }
 });
 
-let SimpleForm = props => {
 
-  const { handleSubmit} = props;
-  //const selector = formValueSelector('SimpleForm');
-  
+let SimpleForm = props => {
+  const { handleSubmit, firstNameValue, lastNameValue } = props;
+
+  console.log({ firstNameValue });
+  console.log({ lastNameValue });
 
   return (
-    <Form onSubmit={handleSubmit}>
-            <Field
-              name="firstName"
-              component={InputField}
-              label="First Name"
-              placeholder="First Name"
-              disabled={true}
-            />
-            <Field
-              name="personalEmail"
-              component={InputField}
-              label="Personal E-Mail"
-              placeholder="Personal E-Mail"
-              disabled={true}
-            />
-            <Field
-              name="phoneNumber"
-              component={InputField}
-              type="number"
-              label="Phone Number"
-              placeholder="Phone Number"
-              disabled={true}
-            />
-            <Field
-              name="personaNumber"
-              component={InputField}
-              type="number"
-              label="Personal Number"
-              placeholder="Personal Number"
-              disabled={true}
-            />
-            <Field
-              name="bankAcount"
-              component={InputField}
-              type="number"
-              label="Bank Acount"
-              placeholder="Bank Acount"
-              disabled={true}
-            />
-            <Field
-              name="startDate"
-              component={InputField}
-              type="date"
-              label="Start Date"
-              placeholder="Start Date"
-              disabled={true}
-            />
-            <Field
-              name="position"
-              component={InputField}
-              label="Position Description"
-              placeholder="Position Description"
-              disabled={true}
-            />
-            <Field
-              name="equipment"
-              component={TextAreaField}
-              label="Equipment"
-              placeholder="Equipment"
-              disabled={true}
-            />
-            <Field
-              name="boss"
-              component={InputField}
-              label="Nearest Boss"
-              placeholder="Nearest boss"
-              disabled={true}
-            />
-            <Field
-              name="registredAD"
-              component={CheckboxField}
-              label="Registred in AD"
-              disabled={true}
-            />
-            <Field
-              name="registredOffice365"
-              component={CheckboxField}
-              label="Registred in Office365"
-              disabled={true}
-            />
-            <Field
-              name="vismaSevera"
-              component={InputField}
-              label="Visma Severa"
-              placeholder="Visma Severa"
-              disabled={true}
-            />
-            <Field
-              name="vismaExpense"
-              component={InputField}
-              label="Visma Expense"
-              placeholder="Visma Expense"
-              disabled={true}
-            />
-            <Field
-              name="vismaSalary"
-              component={InputField}
-              label="Visma Salary"
-              placeholder="Visma Salary"
-              disabled={true}
-            />
-            <Field
-              name="registred"
-              component={CheckboxField}
-              label="Registred in Severa systems"
-              disabled={true}
-            />
-      
-      <PDFDownloadLink document={<MyDoc />} fileName="CheckAccess.pdf">
-        {({  loading }) =>
-          loading ? "Loading document..." : "Download The Form!"
-        }
-      </PDFDownloadLink>
-    </Form>
-    
+    <React.Fragment>
+      <h1>{firstNameValue}</h1>
+      <h1>{lastNameValue}</h1>
+      <Form onSubmit={handleSubmit}>
+        <Field
+          name="firstName"
+          component={InputField}
+          label="First Name"
+          placeholder="First Name"
+          disabled={true}
+        />
+        <Field
+          name="lastName"
+          component={InputField}
+          label="Last Name"
+          placeholder="Last Name"
+          disabled={true}
+        />
+        <Field
+          name="personalEmail"
+          component={InputField}
+          label="Personal E-Mail"
+          placeholder="Personal E-Mail"
+          disabled={true}
+        />
+        <Field
+          name="phoneNumber"
+          component={InputField}
+          type="number"
+          label="Phone Number"
+          placeholder="Phone Number"
+          disabled={true}
+        />
+        <Field
+          name="personaNumber"
+          component={InputField}
+          type="number"
+          label="Personal Number"
+          placeholder="Personal Number"
+          disabled={true}
+        />
+        <Field
+          name="bankAcount"
+          component={InputField}
+          type="number"
+          label="Bank Acount"
+          placeholder="Bank Acount"
+          disabled={true}
+        />
+        <Field
+          name="startDate"
+          component={InputField}
+          type="date"
+          label="Start Date"
+          placeholder="Start Date"
+          disabled={true}
+        />
+        <Field
+          name="position"
+          component={InputField}
+          label="Position Description"
+          placeholder="Position Description"
+          disabled={true}
+        />
+        <Field
+          name="equipment"
+          component={TextAreaField}
+          label="Equipment"
+          placeholder="Equipment"
+          disabled={true}
+        />
+        <Field
+          name="boss"
+          component={InputField}
+          label="Nearest Boss"
+          placeholder="Nearest boss"
+          disabled={true}
+        />
+        <Field
+          name="registredAD"
+          component={CheckboxField}
+          label="Registred in AD"
+          disabled={true}
+        />
+        <Field
+          name="registredOffice365"
+          component={CheckboxField}
+          label="Registred in Office365"
+          disabled={true}
+        />
+        <Field
+          name="vismaSevera"
+          component={InputField}
+          label="Visma Severa"
+          placeholder="Visma Severa"
+          disabled={true}
+        />
+        <Field
+          name="vismaExpense"
+          component={InputField}
+          label="Visma Expense"
+          placeholder="Visma Expense"
+          disabled={true}
+        />
+        <Field
+          name="vismaSalary"
+          component={InputField}
+          label="Visma Salary"
+          placeholder="Visma Salary"
+          disabled={true}
+        />
+        <Field
+          name="registred"
+          component={CheckboxField}
+          label="Registred in Severa systems"
+          disabled={true}
+        />
+        <PDFDownloadLink
+          document={
+            <PDF lnameValue={lastNameValue} fnameValue={firstNameValue} />
+          }
+          fileName="CheckAccess.pdf"
+        >
+          {({ loading }) =>
+            loading ? "Loading document..." : "Download The Form!"
+          }
+        </PDFDownloadLink>
+      </Form>
+    </React.Fragment>
   );
 };
-
-const MyDoc = () => (
-  <Document>
-    <Page size="A4" style={styles.page}>
-      <View style={styles.section}>
-        <Text>form values here</Text>
-      </View>
-    </Page>
-  </Document>
-);
 
 SimpleForm = reduxForm({
   form: "simpleForm",
@@ -170,7 +173,20 @@ SimpleForm = connect(state => ({
     ? state.entities.taskVariables.variables
     : {}
 }))(SimpleForm);
+
+SimpleForm = reduxForm({
+  form: "simpleForm" // a unique identifier for this form
+})(SimpleForm);
+
+const selector = formValueSelector("simpleForm"); // <-- same as form name
+SimpleForm = connect(state => {
+  const firstNameValue = selector(state, "firstName");
+  const lastNameValue = selector(state, "lastName");
+
+  return {
+    firstNameValue,
+    lastNameValue
+  };
+})(SimpleForm);
+
 export default SimpleForm;
-
-
-
